f1ebd24ba1461f567a2a976f2d8bfc5e08a3a8dd
Keks <keks@htmlacademy.ru>
2016-03-12 05:02:05 +0300

    :kissing_cat: Добавил сборку препроцессора в Грант

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+"use strict";
+
+module.exports = function(grunt) {
+  require("load-grunt-tasks")(grunt);
+
+  grunt.initConfig({
+    less: {
+      style: {
+        files: {
+          "css/style.css": "less/style.less"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: [
+            "last 1 version",
+            "last 2 Chrome versions",
+            "last 2 Firefox versions",
+            "last 2 Opera versions",
+            "last 2 Edge versions"
+          ]})
+        ]
+      },
+      style: {
+        src: "css/*.css"
+      }
+    },
+
+    browserSync: {
+      server: {
+        bsFiles: {
+          src: [
+            "*.html",
+            "css/*.css"
+          ]
+        },
+        options: {
+          server: ".",
+          watchTask: true,
+          notify: false,
+          open: true,
+          ui: false
+        }
+      }
+    },
+
+    watch: {
+      files: ["less/**/*.less"],
+      tasks: ["less", "postcss"],
+      options: {
+        spawn: false
+      }
+    }
+  });
+
+  grunt.registerTask("serve", ["browserSync", "watch"]);
+};
